ac97594cea2bf797b3ebc22c03b09153540463df
Vincent Soweto <vincentsoweto22@gmail.com>
2022-04-01 15:19:35 +0200

    first commit of current progress

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Exractor from './components/footer'
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <Extractor />
+    </div>
+  );
+}
+
+export default App;
